1696b2814e958edbc110248cc46a1a22f3008cfe
oppie549 <omprakashbandi583@gmail.com>
2026-09-17 17:49:40 +0530

    fixed the final bugs

---
+++ src/main/java/com/crashdev/restaurant/RestaurantServiceApplication.java
@@ -0,0 +1,11 @@
+package com.crashdev.restaurant;
+
+public final class RestaurantServiceApplication {
+
+    private RestaurantServiceApplication() {
+    }
+
+    public static void main(String[] args) {
+        com.cravedash.restaurant.RestaurantServiceApplication.main(args);
+    }
+}
