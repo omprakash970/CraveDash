6dd278b3bbe3818a56272abd239d367589c273c2
oppie549 <omprakashbandi583@gmail.com>
2026-09-17 16:54:21 +0530

    created service

---
+++ src/main/java/com/crashdev/restaurant/controller/RestaurantController.java
@@ -0,0 +1,30 @@
+package com.crashdev.restaurant.controller;
+
+
+import com.crashdev.restaurant.entity.Restaurant;
+import com.crashdev.restaurant.service.RestaurantService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.net.ssl.HttpsURLConnection;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Restaurant> createRestaurant(@RequestBody Restaurant restaurant){
+        return ResponseEntity.status(HttpsURLConnection.HTTP_CREATED).body(restaurant);
+    }
+
+
+
+}
